788482c829bd731e3cd355ab78f017aae8f61ba6
Popa Gelu <gelu1.popa@gmail.com>
2020-04-23 21:02:07 +0300

    Added Categories List and changed that ugly blue from top bar

---
+++ src/app/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    name: 'Sea',
+    description: 'Warm and windy locations'
+  },
+  {
+    name: 'Mountains',
+    description: 'Cold and windy locations'
+  },
+  {
+    name: 'Danube',
+    description: 'Windy and windy locations'
+  },
+];
