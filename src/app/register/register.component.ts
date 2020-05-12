0a3d0df47d5808e286adb70ecf1decab979cca02
Popa Gelu <gelu1.popa@gmail.com>
2020-05-12 22:15:46 +0300

    doubleWTF

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm;
+  constructor(
+    private formBuilder: FormBuilder,
+    ) {
+    this.registerForm = this.formBuilder.group({
+        username: '',
+        password: '',
+      }
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+}
